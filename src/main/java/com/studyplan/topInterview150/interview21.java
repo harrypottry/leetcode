47b913cf5b74d344adc8c6e2d1040007c541c1d5
harrypottry <122409669@qq.com>
2023-11-19 21:57:03 +0800

    合并链表

--- src/main/java/com/studyplan/topInterview150/interview21.java
+++ src/main/java/com/studyplan/topInterview150/interview21.java
@@ -7,6 +7,12 @@ package com.studyplan.topInterview150;
  */
 public class interview21 {
 
+    /**
+     * 递归实现
+     * @param list1
+     * @param list2
+     * @return
+     */
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return  list2;
         else if (list2 == null) return  list1;
@@ -19,15 +25,39 @@ public class interview21 {
         }
     }
 
+    /**
+     * 迭代实现
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
     public static void main(String[] args) {
         ListNode list1 = new ListNode(1,new ListNode(2,new ListNode(4)));
         ListNode list2 = new ListNode(1,new ListNode(3,new ListNode(4)));
         ListNode listNode = mergeTwoLists(list1, list2);
+        //链表遍历
         while (listNode != null) {
             System.out.println(listNode.val);
             listNode = listNode.next;
         }
     }
-
 }
 
